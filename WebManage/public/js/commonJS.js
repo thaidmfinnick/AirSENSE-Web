af81088e47b695d709dbdeae92de772bbf4de3c2
SuaNgo <okbang20@gmail.com>
2021-10-23 22:28:26 +0700

    undone

--- WebManage/public/js/commonJS.js
+++ WebManage/public/js/commonJS.js
@@ -1,5 +1,4 @@
 
-
 function setItemFormITemAds(item){
     var html=' <div class="sb-img-hust-tech ">  <img class="image-hust-tech" src ="'+item.content_img+'"/>'
                 +'</div>' 
@@ -27,24 +26,23 @@ function setFormToMenuShowAds(dataJson,itemToSet){
 
 
 
-    function setFormToShowPages(item,urlDetail){
-      var start =  '<div class="p-3"><div class="container container-title-hust-tech">';
-      var limkUrl= '<a href="'+urlDetail;
-      if(item.is_main_pages_id==-1){
-        limkUrl += 'group_page/'+item.pages_content_id +'" style="font-size: 20px;">';
-        }
-        else
-        {
-          limkUrl +='detail_page/'+ item.filesave.replace('/', '+')+' " style="font-size: 20px;">';
-        }
-      
-      var content = ' <div class="row align-items-center">  <div class="col-lg-3"> <div class="img-part js-tilt">'           
-                 + '<img src="' +item.content_img +'" alt="images"  style="width: 60%"/>  </div> </div>'
-                 +'<div class="col-lg-9"> <div class="div-font-title-hust-tech">'+item.title +'</div> <div class="content div-font-info-hust-tech "><br/>' +item.content
-                 + '</div></div></div></div></div>';
-
-      return (limkUrl +start+content + ' </a>');
-    }
+  function setFormToShowPages(item,urlDetail){
+    var start =  '<div class="row content2">';
+    var limkUrl= '<a href="'+urlDetail;
+    if(item.is_main_pages_id==-1){
+      limkUrl += 'group_page/'+item.pages_content_id +'" style="font-size: 20px;">';
+      }
+      else
+      {
+        limkUrl +='detail_page/'+ item.filesave.replace('/', '+')+' " style="font-size: 20px;">';
+      }
+    
+      var content = ' <div class="col-2"></div><div class="col-10"><div class="row"><div class="col-sm-4 col-md-4"><div class="card">'           
+      + '<img src="' +item.content_img +'" alt="images"  class="card-img-top"/> <div class="card-body">'
+      +'<h5 class="card-title">'+item.title +'</h5> <p class="card-text">' +item.content
+      + '</p><a href="'+urlDetail+'" class="btn btn-primary">Xem thêm</a></div></div></div></div></div></div></div>';
+    return (start+content);
+  }
 
 
     function  getInfoDetailPages(nameDivControl,dataView,urlDetail){
@@ -102,5 +100,3 @@ function setFormToMenuShowAds(dataJson,itemToSet){
         },
       });
     }
-
-
